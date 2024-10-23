62c9518e878356becc1e7503f7f9475df3c410ff
Brian Ott <bott@b1-systems.de>
2024-10-23 11:28:06 +0200

    update: fmt

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,23 +1,23 @@
 package auth
 
 import (
-    "net/http"
-    "testing"
+	"net/http"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-    req, err := http.NewRequest("GET", "/test", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    req.Header.Set("Authorization", "ApiKey abcd1234")
+	req, err := http.NewRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "ApiKey abcd1234")
 
-    apiKey, err := GetAPIKey(req.Header)
-    if err != nil {
-        t.Fatal(err)
-    }
+	apiKey, err := GetAPIKey(req.Header)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    if apiKey != "abcd1234" {
-        t.Errorf("Expected API key to be 'abcd1234', got %v", apiKey)
-    }
+	if apiKey != "abcd1234" {
+		t.Errorf("Expected API key to be 'abcd1234', got %v", apiKey)
+	}
 }
